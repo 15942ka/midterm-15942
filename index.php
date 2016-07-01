d25e3ba0700702841b7cbe2cdfebe7b61d548cca
15942ka <ajaykak999@gmail.com>
2016-06-30 17:08:36 -0700

    Update index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+<br>
+<h1 > Ajaykumar Kondragunta - 15942 </h1>
+<table align=absleft border=1 cellspacing= 1 cellpadding=1 width=180 height=180>
+   <tr>
+		<th colspan=2 rowspan=1>A</th>
+		<th colspan=1 rowspan=1>B</th>
+   </tr>
+	<tr>
+		<th id="tb" colspan="3" rowspan="1">C</th>
+	</tr>
+   
+   <tr>
+		<th colspan=1 rowspan=1>D</th>
+		<th colspan=1 rowspan=1>E</th>
+   </tr>
+
+  
+</table>	
+</body>	
+</html>
